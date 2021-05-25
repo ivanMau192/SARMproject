1c67cb6483fef041056e15f4adb34b9f4595f4be
Ivan Maulen <imaulen@movilform.com>
2021-05-24 22:35:41 -0400

    Se añade vista para login con validacion de datos

---
+++ SarmFrontend/src/app/components/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+export class EmailErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+export class PasswordErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  emailMatcher = new EmailErrorStateMatcher();
+  passwordMatcher = new PasswordErrorStateMatcher();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    console.log(this.emailFormControl)
+    console.log(this.passwordFormControl)
+  }
+
+}
