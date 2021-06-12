2598663f773e92df19ac69423f460d03be42b90d
Ivan Maulen <imaulen@movilform.com>
2021-06-12 16:25:45 -0400

    Se agrega permiso a modulo de usuarios

--- SarmFrontend/src/app/components/initial-view/initial-view.component.ts
+++ SarmFrontend/src/app/components/initial-view/initial-view.component.ts
@@ -14,12 +14,24 @@ export class InitialViewComponent implements OnInit {
   @Output() loginChanged: EventEmitter<Object> = new EventEmitter()
   user: string;
   renderUserName;
+  permissions: any;
 
 
   constructor(private loginService:LoginService) { }
 
+  checkPerm(perm){
+    
+    if(this.permissions.includes(perm)){
+      return true
+    }else{
+      return false
+    }
+  }
+
+
   ngOnInit(): void {
     this.user = JSON.parse(localStorage.getItem('user'))
+    this.permissions = this.user["permissions"]
     this.renderUserName = this.user["user"].user_username;
     console.log(this.user)
   }
